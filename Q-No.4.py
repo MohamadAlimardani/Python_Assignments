29ac3452a4239c2a6df17b3447819c2b268192c5
Mohamad Alimardani <107054489+USSDHDD@users.noreply.github.com>
2022-08-04 20:05:15 +0430

    Delete Q-No.4.py

--- Q-No.4.py
+++
@@ -1,8 +0,0 @@
-
-height = int(input("Enter you're Desired Height: "))
-base = int(input("Enter you're Desired Base: "))
-
-area = (height * base)/2
-format_area = "{:.2f}".format(area)
-
-print("Area of you're Triangle is: " , format_area)
