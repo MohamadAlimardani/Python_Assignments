51062fe812d925c06c9bbd42b2d00ea34d39dd43
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-08-14 13:49:53 +0430

    Update multiple of 7.py

--- Assignment 2/multiple of 7.py
+++ Assignment 2/multiple of 7.py
@@ -1,10 +1,9 @@
 
 number = int(input("Enter you're Desired Number: "))
-const = (0 , 7)
 
-if number % const[1] == const[0]:
+if number % 7 == 0:
     print("The Given number is a multiple of 7. ")
 
 else:
-    next_number = number + (const[1] - number % const[1])
+    next_number = number + (7 - number % 7)
     print("The Given number is not a multiple of 7. " , "\nThe Next Number that is a multiple of 7 is " + str(next_number) + ".")
