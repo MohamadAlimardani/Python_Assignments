7051570de9eafdda27ccd91e5f0f33ef8e2e6709
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-08-14 13:52:57 +0430

    Update Temperature Conversion.py

--- Assignment 2/Temperature Conversion.py
+++ Assignment 2/Temperature Conversion.py
@@ -1,26 +1,25 @@
 
 while True:
-    const = (1 , 2 , 3 , 4 , 5 , 6 , 32 , 273.15 , 5/9 , 9/5)
-    print("Hi! \n-Choose an option from below: " , 
+    print("Hi! \n-Choose an option from below: \n" , 
     "\n1.Fahrenheit to Celsius. " , 
     "\n2.Celsius to Fahrenheit. " , 
     "\n3.Kelvin to Celsius. " , 
     "\n4.Celsius to Kelvin. " , 
     "\n5.Kelvin to Fahrenheit. " , 
-    "\n6.Fahrenheit to Kelvin. ")
+    "\n6.Fahrenheit to Kelvin. \n")
     number_of_option = int(input("-Waiting for you're Desire: "))
 
-    if number_of_option == const[0]:
+    if number_of_option == 1:
         Fahrenheit = float(input("Enter You're Desired Fahrenheit Temperature: "))
-        F_to_C = (Fahrenheit - const[6]) * (const[8])
+        F_to_C = (Fahrenheit - 32) * (5/9)
         format_F_to_C = "{:.2f}".format(F_to_C)
         print("You're Conversion from Fahrenheit to Celsius is: " , format_F_to_C)
         while True:
             print("-Choose an option from below: " , "\n1.Back to Main Menu! " , "\n2.Exit! ")    
             num_continue = int(input("-Waiting for you're Desire: "))
-            if num_continue == const[0]:
+            if num_continue == 1:
                 break
-            elif num_continue == const[1]:
+            elif num_continue == 2:
                 exit = True
                 break
             else:
@@ -32,17 +31,17 @@ while True:
         else:
             continue
     
-    elif number_of_option == const[1]:
+    elif number_of_option == 2:
         Celsius = float(input("Enter You're Desired Celsius Temperature: "))
-        C_to_F = (Celsius * (const[9])) + const[6]
+        C_to_F = (Celsius * (9/5)) + 32
         format_C_to_F = "{:.2f}".format(C_to_F)
         print("You're Conversion from Celsius to Fahrenheit is: " , format_C_to_F)
         while True:
             print("-Choose an option from below: " , "\n1.Back to Main Menu! " , "\n2.Exit! ")    
             num_continue = int(input("-Waiting for you're Desire: "))
-            if num_continue == const[0]:
+            if num_continue == 1:
                 break
-            elif num_continue == const[1]:
+            elif num_continue == 2:
                 exit = True
                 break
             else:
@@ -54,17 +53,17 @@ while True:
         else:
             continue
     
-    elif number_of_option == const[2]:
+    elif number_of_option == 3:
         Kelvin = float(input("Enter You're Desired Kelvin Temperature: "))
-        K_to_C = Kelvin - const[7]
+        K_to_C = Kelvin - 273.15
         format_K_to_C = "{:.2f}".format(K_to_C)
         print("You're Conversion from Kelvin to Celsius is: " , format_K_to_C)
         while True:
             print("-Choose an option from below: " , "\n1.Back to Main Menu! " , "\n2.Exit! ")    
             num_continue = int(input("-Waiting for you're Desire: "))
-            if num_continue == const[0]:
+            if num_continue == 1:
                 break
-            elif num_continue == const[1]:
+            elif num_continue == 2:
                 exit = True
                 break
             else:
@@ -76,17 +75,17 @@ while True:
         else:
             continue
     
-    elif number_of_option == const[3]:
+    elif number_of_option == 4:
         Celsius = float(input("Enter You're Desired Celsius Temperature: "))
-        C_to_K = Celsius + const[7]
+        C_to_K = Celsius + 273.15
         format_C_to_K = "{:.2f}".format(C_to_K)
         print("You're Conversion from Celsius to Kelvin is: " , format_C_to_K)
         while True:
             print("-Choose an option from below: " , "\n1.Back to Main Menu! " , "\n2.Exit! ")    
             num_continue = int(input("-Waiting for you're Desire: "))
-            if num_continue == const[0]:
+            if num_continue == 1:
                 break
-            elif num_continue == const[1]:
+            elif num_continue == 2:
                 exit = True
                 break
             else:
@@ -98,17 +97,17 @@ while True:
         else:
             continue
     
-    elif number_of_option == const[4]:
+    elif number_of_option == 5:
         Kelvin = float(input("Enter You're Desired Kelvin Temperature: "))
-        K_to_F = (Kelvin - const[7]) * (const[9]) + const[6]
+        K_to_F = (Kelvin - 273.15) * (9/5) + 32
         format_K_to_F = "{:.2f}".format(K_to_F)
         print("You're Conversion from Kelvin to Fahrenheit is: " , format_K_to_F)
         while True:
             print("-Choose an option from below: " , "\n1.Back to Main Menu! " , "\n2.Exit! ")    
             num_continue = int(input("-Waiting for you're Desire: "))
-            if num_continue == const[0]:
+            if num_continue == 1:
                 break
-            elif num_continue == const[1]:
+            elif num_continue == 2:
                 exit = True
                 break
             else:
@@ -120,17 +119,17 @@ while True:
         else:
             continue
     
-    elif number_of_option == const[5]:
+    elif number_of_option == 6:
         Fahrenheit = float(input("Enter You're Desired Fahrenheit Temperature: "))
-        F_to_K = (Fahrenheit - const[6]) * (const[8]) + const[7]
+        F_to_K = (Fahrenheit - 32) * (5/9) + 273.15
         format_F_to_K = "{:.2f}".format(F_to_K)
         print("You're Conversion from Fahrenheit to Kelvin is: " , format_F_to_K)
         while True:
             print("-Choose an option from below: " , "\n1.Back to Main Menu! " , "\n2.Exit! ")    
             num_continue = int(input("-Waiting for you're Desire: "))
-            if num_continue == const[0]:
+            if num_continue == 1:
                 break
-            elif num_continue == const[1]:
+            elif num_continue == 2:
                 exit = True
                 break
             else:
