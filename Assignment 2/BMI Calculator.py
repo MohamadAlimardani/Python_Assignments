02d5982da2ace58575b534423a8c2b44daad7f5a
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-08-14 13:55:51 +0430

    Update BMI Calculator.py

--- Assignment 2/BMI Calculator.py
+++ Assignment 2/BMI Calculator.py
@@ -8,27 +8,27 @@ while True:
     format_bmi = "{:.2f}".format(bmi)
 
     if 16 <= bmi < 18.5:
-        print("You're BMI is " + str(format_bmi) + ", You should Gain some weight Asap. ")
+        print("You're BMI is " + format_bmi + ", You should Gain some weight Asap. ")
         break
 
     elif 18.5 <= bmi < 24:
-        print("You're BMI is " + str(format_bmi) + ", Congrats! You're weight is Normal. ")
+        print("You're BMI is " + format_bmi + ", Congrats! You're weight is Normal. ")
         break
 
     elif 24 <= bmi < 30:
-        print("You're BMI is " + str(format_bmi) + ", You're slightly Overweight , Watch out You're Diet. ")
+        print("You're BMI is " + format_bmi + ", You're slightly Overweight , Watch out You're Diet. ")
         break
 
     elif 30 <= bmi < 35:
-        print("You're BMI is " + str(format_bmi) + ", You've got first Degree Obesity , You should go on a Diet. ")
+        print("You're BMI is " + format_bmi + ", You've got first Degree Obesity , You should go on a Diet. ")
         break
 
     elif 35 <= bmi < 40:
-        print("You're BMI is " + str(format_bmi) + ", You've got second Degree Obesity , You have to go on a Diet , It's almost Dangerous!")
+        print("You're BMI is " + format_bmi + ", You've got second Degree Obesity , You have to go on a Diet , It's almost Dangerous!")
         break
 
     elif bmi >= 40:
-        print("You're BMI is " + str(format_bmi) + ", You've got third Degree Obesity , You have to go on a Diet , You're Health care is in Danger! ")
+        print("You're BMI is " + format_bmi + ", You've got third Degree Obesity , You have to go on a Diet , You're Health care is in Danger! ")
         break
     else:
         print("Invalid Information due to super low BMI!\nPlease Enter again.")
