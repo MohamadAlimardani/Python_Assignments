25360f3f3b2f9d197692eea3faefc933d4cc4039
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-08-14 13:48:37 +0430

    Update Log-in.py

--- Assignment 2/Log-in.py
+++ Assignment 2/Log-in.py
@@ -2,16 +2,14 @@
 static_username = "Alimardani"
 static_password = 12345
 
-const = (0 , 1 , 2 , 3)
+loop_counter = 0
+counter_tries = 0
+counter_limit = 0
 
-loop_counter = const[0]
-counter_tries = const[0]
-counter_limit = const[0]
+while loop_counter < 3:
 
-while loop_counter < const[3]:
-
-    loop_counter += const[1]
-    counter_tries = const[3] - loop_counter
+    loop_counter += 1
+    counter_tries = 3 - loop_counter
 
     dynamic_username = input("Enter you're Username: ")
     dynamic_password = int(input("Enter you're Password: "))
@@ -21,15 +19,15 @@ while loop_counter < const[3]:
         break
     
     elif static_username != dynamic_username or static_password != dynamic_password:
-        counter_limit += const[1]
-        if const[2] <= counter_tries <= const[3]:
+        counter_limit += 1
+        if 2 <= counter_tries <= 3:
             print("Username or Password is Wrong! " , "\nYou've got only \"" , counter_tries , "\" more tries. ")
             continue
-        elif counter_tries == const[1]:
+        elif counter_tries == 1:
             print("You've got only \"" , counter_tries , "\" more try. ")
             continue
         else:
             print("You've got no more tries. ")
 
-if counter_limit == const[3]:
+if counter_limit == 3:
     print("You're Account has been Locked due to Entering wrong informations. ")
