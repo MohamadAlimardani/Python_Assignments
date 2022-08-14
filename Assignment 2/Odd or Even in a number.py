854989b87bbda21cfa48df1a7d2617e72ff712e6
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-08-14 13:50:45 +0430

    Update Odd or Even in a number.py

--- Assignment 2/Odd or Even in a number.py
+++ Assignment 2/Odd or Even in a number.py
@@ -1,19 +1,18 @@
 
 number = int(input("Enter your Desired number: "))
 
-const = (0 , 1 , 2 , 10)
-even_counter = const[0]
-odd_counter = const[0]
+even_counter = 0
+odd_counter = 0
 
-while number > const[0]:
-    remainder = number % const[3]
-    number = number // const[3]
+while number > 0:
+    remainder = number % 10
+    number = number // 10
 
-    if remainder % const[2] == const[0]:
-        even_counter += const[1]
+    if remainder % 2 == 0:
+        even_counter += 1
 
     else:
-        odd_counter += const[1]
+        odd_counter += 1
 
 if even_counter > odd_counter:
     print("\nEven = " , even_counter , "\nODD = " , odd_counter , "\nThere is more Even Digits than ODD Digits in the given number. ")
