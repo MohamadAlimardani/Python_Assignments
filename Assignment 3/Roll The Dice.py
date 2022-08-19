55ba4c1010c25492ee0f5316684b49b0e92dbc79
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-08-20 00:24:34 +0430

    Update Roll The Dice.py

--- Assignment 3/Roll The Dice.py
+++ Assignment 3/Roll The Dice.py
@@ -5,7 +5,8 @@ dice_number = []
 while True:
     dice_number.append(ran.randint(0 , 6))
     if 6 in dice_number:
-        print("\n\nSum of All Pulled Numbers: " + str(sum(dice_number)))
+        print("\n\nSum of All Pulled Numbers: " + str(sum(dice_number)) 
+        + "\n\nList Of Numbers That We Pulled from Rolling The Dice: \n" + str(dice_number))
         break
     
     else:
