b40e7a1132ccd6e83ad2d181be50636195e4c3b0
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-08-22 10:02:45 +0430

    Update AI Guessing Number.py

--- Assignment 3/AI Guessing Number.py
+++ Assignment 3/AI Guessing Number.py
@@ -17,28 +17,33 @@ expected_number = ran.randint(first_interval , end_interval)
 numbers =[]
 
 while True:
-    print("\nExpected Number: " + str(expected_number) + "\n")
-    while True:
-        AI_guess = ran.randint(first_interval , end_interval)
+    Try:
+        print("\nExpected Number: " + str(expected_number) + "\n")
+        while True:
+            AI_guess = ran.randint(first_interval , end_interval)
 
-        if AI_guess in numbers:
-            continue
-        
-        elif AI_guess not in numbers:
-            numbers.append(AI_guess)
+            if AI_guess in numbers:
+                continue
+
+            elif AI_guess not in numbers:
+                numbers.append(AI_guess)
+                break
+
+        if AI_guess == expected_number:
+            End_Print()
             break
 
-    if AI_guess == expected_number:
-        End_Print()
-        break
-    
-    Print_Smaller_Greater_Menu()
-    small_great_meanu = input("-Waiting For You're Desire: ")
-    
-    if small_great_meanu == "1":
-        end_interval = AI_guess
-        continue
-    
-    elif small_great_meanu == "2":
-        first_interval = AI_guess
+        Print_Smaller_Greater_Menu()
+        small_great_meanu = input("-Waiting For You're Desire: ")
+
+        if small_great_meanu == "1":
+            end_interval = AI_guess
+            continue
+
+        elif small_great_meanu == "2":
+            first_interval = AI_guess
+            continue
+    except:
+        print("Unfortunately!! We Ran into Some Errors.\n\nRepeat The Guessing Process Again.")
+        numbers.clear()
         continue
