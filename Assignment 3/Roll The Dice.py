713653e47173d1ed7ef262d57937004014ca6e3d
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-08-24 13:05:50 +0430

    Update Roll The Dice.py

--- Assignment 3/Roll The Dice.py
+++ Assignment 3/Roll The Dice.py
@@ -3,13 +3,14 @@ import random as ran
 
 dice_number = []
 while True:
-    dice_number.append(ran.randint(1 , 6))
-    if 6 in dice_number:
+    dice = ran.randint(1 , 6)
+    dice_number.append(dice)  
+    if dice == 6:
+        continue
+    
+    else:
         print("\n\nSum of All Pulled Numbers: " + str(sum(dice_number)) 
         + "\n\nList Of Numbers That We Pulled from Rolling The Dice: \n" + str(dice_number))
         break
-    
-    else:
-        continue
 
-print("\nNumber of Tries To Get Number '6': " + str(len(dice_number)-1) + "\n\n")
+print("\nNumber of Tries: " + str(len(dice_number)) + "\n\n")
