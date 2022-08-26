356f63a25d7934084311cf34d518ef1eefa22a63
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-08-26 14:52:27 +0430

    Update Qr Code Generator.py

--- Assignment 4/Qr Code Generator.py
+++ Assignment 4/Qr Code Generator.py
@@ -13,9 +13,9 @@ while True:
     qr = qc.QRCode(version = rnd.randint(1 , 40) , error_correction = qc.ERROR_CORRECT_L 
                    , box_size = rnd.randint(1 , 15) , border = rnd.randint(1 , 15))
 
-    qr.add_data(data)
-    qr.make(fit = True)
+    qc.add_data(data)
+    qc.make(fit = True)
 
-    img = qr.make_image(fill_color = (rnd.randint(0 , 255) , rnd.randint(0 , 255) , rnd.randint(0 , 255)) 
+    img = qc.make_image(fill_color = (rnd.randint(0 , 255) , rnd.randint(0 , 255) , rnd.randint(0 , 255)) 
                         , back_color = (rnd.randint(0 , 255) , rnd.randint(0 , 255) , rnd.randint(0 , 255)))
     img.save("Qr-Code.JPG")
