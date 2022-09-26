60ffbd4c5f81476b86d1a5fe9f317e41593389e0
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-09-26 14:32:23 +0330

    Update Calculating Something.py

--- Assignment 7/Calculating Something.py
+++ Assignment 7/Calculating Something.py
@@ -1,9 +1,4 @@
 
 num = input("Enter You're Desired Number: ")
 
-num1 = num * 2
-num2 = num * 3
-
-res  = int(num) + int(num1) + int(num2)
-
-print(f"{num} + {num1} + {num2} = {res}")
+print(f"{num} + {num * 2} + {num * 3} = {int(num) + int(num * 2) + int(num * 3)}")
