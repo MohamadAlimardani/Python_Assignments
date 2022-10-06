e5aed39a9e1cecea282c4279db18c6e99199630e
Mohamad Alimardani <107054489+MohamadAlimardani@users.noreply.github.com>
2022-10-07 01:02:16 +0330

    Update 2D-Multiplication.py

--- Assignment 7/2D-Multiplication.py
+++ Assignment 7/2D-Multiplication.py
@@ -1,7 +1,7 @@
 
-rows_cols = int(input("Enter a Number \'?x?\': "))
+rows, cols = int(input("Enter Rows: ")), int(input("Enter Columns: "))
 
-for i in range(1, rows_cols + 1):
-    for j in range(1, rows_cols + 1):
+for i in range(1, rows + 1):
+    for j in range(1, cols + 1):
         print(i * j , end = "\t")
     print(2 * "\n")
